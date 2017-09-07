28ebc6375a0e2c5cbbc40067e6785945d2a7a15f
jerry <jiangchi_1991@sina.com>
2017-09-07 14:34:50 +0800

    main js

---
+++ math.js
@@ -0,0 +1,137 @@
+var mathCalc = function() {
+    
+    /**
+     * JS 数字丢失精度的原因：
+     *计算机的二进制实现和位数限制有些数无法有限表示。就像一些无理数不能有限表示，如 圆周率 3.1415926...，1.3333... 等。
+     * JS 遵循 IEEE 754规范，采用双精度存储（double precision），占用 64 bit
+     * 浮点数，比如
+     * 
+			0.1 >> 0.0001 1001 1001 1001…（1001无限循环）
+			0.2 >> 0.0011 0011 0011 0011…（0011无限循环）
+			
+		此时只能模仿十进制进行四舍五入了，但是二进制只有 0 和 1 两个，于是变为 0 舍 1 入。这即是计算机中部分浮点数运算时出现误差，丢失精度的根本原因。
+		大整数的精度丢失和浮点数本质上是一样的，尾数位最大是 52 位，因此 JS 中能精准表示的最大整数是 Math.pow(2, 53)，十进制即 9007199254740992。
+		大于 9007199254740992 的可能会丢失精度	
+		这篇文章有具体介绍http://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+		
+		解决方案: 
+		对于整数，前端出现问题的几率可能比较低，毕竟很少有业务需要需要用到超大整数，只要运算结果不超过 Math.pow(2, 53) 就不会丢失精度。
+		对于小数，前端出现问题的几率还是很多的，尤其在一些电商网站涉及到金额等数据。解决方式：把小数放到位整数（乘倍数），再缩小回原来倍数（除倍数）
+     */
+    
+    
+    /*
+     * 判断obj是否为一个整数
+     */
+    function isInteger(obj) {
+        return Math.floor(obj) === obj
+    }
+    
+    /*
+     * 将一个浮点数转成整数，返回整数和倍数。如 3.14 >> 314，倍数是 100
+     * @param floatNum {number} 小数
+     * @return {object}
+     *   {times:100, num: 314}
+     */
+    function toInteger(floatNum) {
+        var ret = {times: 1, num: 0}
+        if (isInteger(floatNum)) {
+            ret.num = floatNum
+            return ret
+        }
+        var strfi  = floatNum + ''
+        var dotPos = strfi.indexOf('.')
+        var len    = strfi.substr(dotPos+1).length
+        var times  = Math.pow(10, len)
+        var intNum = parseInt(floatNum * times + 0.5, 10)
+        ret.times  = times
+        ret.num    = intNum
+        return ret
+    }
+    
+    function toFixed(num, s) {
+	    var times = Math.pow(10, s)
+	    var des = num * times + 0.5
+	    des = parseInt(des, 10) / times
+	    return des + ''
+	}
+    /*
+     * 核心方法，实现加减乘除运算，确保不丢失精度
+     * 思路：把小数放大为整数（乘），进行算术运算，再缩小为小数（除）
+     * @param a {number} 运算数1
+     * @param b {number} 运算数2
+     * @param op {string} 运算类型，有加减乘除（add/subtract/multiply/divide）
+     *
+     */
+    function operation(a, b, op) {
+        var o1 = toInteger(a)
+        var o2 = toInteger(b)
+        var n1 = o1.num
+        var n2 = o2.num
+        var t1 = o1.times
+        var t2 = o2.times
+        var max = t1 > t2 ? t1 : t2
+        var result = null
+        switch (op) {
+            case 'add':
+                if (t1 === t2) { // 两个小数位数相同
+                    result = n1 + n2
+                } else if (t1 > t2) { // o1 小数位 大于 o2
+                    result = n1 + n2 * (t1 / t2)
+                } else { // o1 小数位 小于 o2
+                    result = n1 * (t2 / t1) + n2
+                }
+                return result / max
+            case 'subtract':
+                if (t1 === t2) {
+                    result = n1 - n2
+                } else if (t1 > t2) {
+                    result = n1 - n2 * (t1 / t2)
+                } else {
+                    result = n1 * (t2 / t1) - n2
+                }
+                return result / max
+            case 'multiply':
+                result = (n1 * n2) / (t1 * t2)
+                return result
+            case 'divide':
+                result = (n1 / n2) * (t2 / t1)
+                return result
+        }
+    }
+    
+    // 加减乘除的四个接口
+    function add(a, b) {
+        return operation(a, b, 'add')
+    }
+    function subtract(a, b) {
+        return operation(a, b, 'subtract')
+    }
+    function multiply(a, b, digits) {
+        return operation(a, b,'multiply')
+    }
+    function divide(a, b, digits) {
+        return operation(a, b, 'divide')
+    }
+    
+    Number.prototype.add = function (a) {
+    	return add.apply(null, [this, a]);
+    }
+    Number.prototype.subtract = function (a) {
+    	return subtract.apply(null, [this, a]);
+    }
+    Number.prototype.multiply = function (a) {
+    	return multiply.apply(null, [this, a]);
+    }
+    Number.prototype.divide = function (a) {
+    	return divide.apply(null, [this, a]);
+    }    
+	// export 
+    return {
+        add: add,
+        subtract: subtract,
+        multiply: multiply,
+        divide: divide,
+        tofixed: toFixed
+    }
+}();
